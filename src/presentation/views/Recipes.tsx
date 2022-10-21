7585fb6c676a58deb7bd9148a5681ffd34dca3be
oscargps <oscar9557@hotmail.com>
2022-10-21 08:17:46 -0500

    ✨ FEAT: Recipes View

---
+++ src/presentation/views/Recipes.tsx
@@ -0,0 +1,11 @@
+import '../assets/css/recipes.scss'
+
+const Recipes = () => {
+    return (
+        <>
+         <div className="recipesTitle">Nuevas Recetas</div>
+        </>
+    )
+}
+
+export default Recipes;
